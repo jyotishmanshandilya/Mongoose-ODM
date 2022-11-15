70607f36a75682dbfdffb7e37b69b5b9b0af226d
jyotishman <jyotishmanshandilyaggn@gmail.com>
2022-11-15 22:47:08 +0530

    mongoose v2

--- index.js
+++ index.js
@@ -7,23 +7,22 @@ const connect = mongoose.connect(url);
 
 connect.then((db)=>{
     console.log("Connnected correctly to the server");
-    var newDish = Dishes({
+    Dishes.create({
         name:"Pasta",
         description:"High in fats"
+    })
+    .then((dish)=>{
+        console.log(dish);
+        return Dishes.find({}).exec();
+    })
+    .then((dishes)=>{
+        console.log(dishes);
+        return Dishes.remove({});
+    })
+    .then(()=>{
+        return mongoose.connection.close();
+    })
+    .catch((err)=>{
+        console.log(err);
     });
-    newDish.save()
-        .then((dish)=>{
-            console.log(dish);
-            return Dishes.find({}).exec();
-        })
-        .then((dishes)=>{
-            console.log(dishes);
-            return Dishes.remove({});
-        })
-        .then(()=>{
-            return mongoose.connection.close();
-        })
-        .catch((err)=>{
-            console.log(err);
-        });
 });
