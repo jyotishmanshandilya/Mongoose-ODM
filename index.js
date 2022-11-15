bfaa92a805359625d6bf2a37e4e70efbe4b0e06e
jyotishman <jyotishmanshandilyaggn@gmail.com>
2022-11-15 23:19:08 +0530

    Mongoose ODM v3

--- index.js
+++ index.js
@@ -8,15 +8,29 @@ const connect = mongoose.connect(url);
 connect.then((db)=>{
     console.log("Connnected correctly to the server");
     Dishes.create({
-        name:"Pasta",
+        name:"Milkshake",
         description:"High in fats"
     })
     .then((dish)=>{
         console.log(dish);
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set:{description:'Updated Test'}
+        },{
+             new: true 
+        })
+        .exec();
     })
-    .then((dishes)=>{
-        console.log(dishes);
+    .then((dish)=>{
+        console.log(dish);
+        dish.comments.push({
+            rating:5,
+            comment: "I'm very sleepy",
+            author: "andrew tate"
+        });
+        return dish.save();
+    })
+    .then((dish)=>{
+        console.log("This is dish: "+dish);
         return Dishes.remove({});
     })
     .then(()=>{
